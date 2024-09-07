ac666a4c0692c714463d567770d1349a9b76fd68
Harsh Tenguriya <tengo.git@tenguriya.com>
2024-09-07 21:57:19 +0530

    Prompt Based chat GPT chaining and model chating

--- typescript/langchain-project-openai-1/src/index.ts
+++ typescript/langchain-project-openai-1/src/index.ts
@@ -1,21 +1,43 @@
 import { ChatOpenAI } from "@langchain/openai";
-import { AIMessageChunk, HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { configDotenv } from "dotenv";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 configDotenv();
 
 const model: ChatOpenAI = new ChatOpenAI({
-  model: "gpt-4",
+    model: "gpt-4o-mini",
 });
+const parser: StringOutputParser = new StringOutputParser();
 
-const messages: (SystemMessage | HumanMessage)[] = [
-    new SystemMessage("Translate the following from English into Italian"),
-    new HumanMessage("hi!"),
-]
+// const messages: (SystemMessage | HumanMessage)[] = [
+//     new SystemMessage("Behave Like A Friend, Keep all the response small. Am developing the AI model"),
+//     new HumanMessage("hi!"),
+// ]
 
-model.invoke(messages).then(async (res: AIMessageChunk) => {
-    const parser: StringOutputParser = new StringOutputParser();
-    const response: string = await parser.invoke(res);
-    console.log("Ginnie : ", response);
-});
+const promptTemplate = ChatPromptTemplate.fromMessages([
+    [
+        "system",
+        "Behave Like A Friend, Keep all the response small. Am developing the AI model with language {language}.",
+    ],
+    ["user", "{text}"],
+]);
+
+const chain = promptTemplate.pipe(model).pipe(parser).pipe(printMessage);
+
+chain
+    .invoke({
+        text: "hi!",
+        language: "English",
+    })
+    .then(() =>
+        chain.invoke({
+            language: "English",
+            text: "What is the temprature of jaipur today?",
+        }),
+    );
+
+function printMessage(message: string) {
+    console.log(`Ginnie : ${message}`);
+}
